c407babdfc09f633fef34a1378c649fa53904726
Mesiahs <sebastianmesias04@gmail.com>
2025-02-18 21:52:29 -0500

    acomodar el boton con el loading

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,10 +1,11 @@
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
 import logo_lukxtec from 'assets/img/lukxtec.jpg'
+import loading_dots from 'assets/img/loading-dots.gif'
 
 function Navbar(){
     return (
-        <nav className='w-full py-10 shadow-md fixed'>
+        <nav className='w-full py-6 shadow-md fixed'>
             <div className="bg-white px-4 sm:px-6">
             <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap md:px-14 px-2">
                 <div className="ml-4 mt-2">
@@ -20,9 +21,10 @@ function Navbar(){
                 <Link to='/Nosotros' className="text-lg inline-flex  font-medium leading-6 text-gray-900 mx-4">Nosotros</Link>    
 Link         <button
                     type="button"
-                    className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-orange-button px-6 py-2 text-lg font-bold text-white shadow-sm transition duration-300 ease-in-out hover:bg-black focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
                 >
-                    Create new job
+                    Iniciar test
+                    <img src= {loading_dots} className='w-12 h-4 mt-1 ml-2'/>
                 </button>
                 </div>
             </div>
