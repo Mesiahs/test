7c61aa27819f5b50fe48383f2a6d3f3e5e41fc65
Mesiahs <sebastianmesias04@gmail.com>
2025-02-18 22:39:47 -0500

    acomodando las direcciones de los path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Home from 'containers/pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import store from './store';
 import { Provider } from 'react-redux';
+import Cases from 'containers/pages/Cases';
+import Services from 'containers/pages/Services';
+import About from 'containers/pages/About';
+import Blog from 'containers/pages/Blog';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
 
           {/* Home Display*/}
           <Route path="/" element={<Home />} />
+          <Route path="/Casos" element={<Cases />} />
+          <Route path="/Servicios" element={<Services />} />
+          <Route path="/Nosotros" element={<About />} />
+          <Route path="/Blog" element={<Blog />} />
         </Routes>
       </Router>
     </Provider>
