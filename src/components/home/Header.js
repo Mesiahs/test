7133d9da648bf1ce17dfa8f549580d8bd46dd8a6
Mesiahs <sebastianmesias04@gmail.com>
2025-03-16 15:22:51 -0500

    se coloca unos items en header, no sirven para hacer click, sigue el nav bar

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,48 +1,47 @@
+import { Typewriter } from "react-simple-typewriter"
+import { Link } from 'react-router-dom'
+
 function Header(){
-    return(
-        <main>
+
+  return(
+      <main>
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
             <div>
-              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                  <span className="text-gray-600">
-                    Announcing our next round of funding.{' '}
-                    <a href="#" className="font-semibold text-indigo-600">
-                      <span className="absolute inset-0" aria-hidden="true" />
-                      Read more <span aria-hidden="true">&rarr;</span>
-                    </a>
-                  </span>
-                </div>
-              </div>
+              
               <div>
                 <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                  Data to enrich your online business
+                  Data to <span> </span>
+                    <div className='inlie-flex' style={{color: 'blue', fontWeight: 'bold'}}>
+
+                      <Typewriter
+                        words={['Action', 'Insight', 'Innovation']}
+                        loop={0}
+                        cursor
+                        cursorStyle='_'
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+
+                      />
+                  </div>  
                 </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                  amet fugiat veniam occaecat fugiat aliqua.
-                </p>
-                <div className="mt-8 flex gap-x-4 sm:justify-center">
-                  <a
-                    href="#"
-                    className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-                  >
-                    Get started
-                    <span className="text-indigo-200" aria-hidden="true">
-                      &rarr;
-                    </span>
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                  >
-                    Live demo
-                    <span className="text-gray-400" aria-hidden="true">
-                      &rarr;
-                    </span>
-                  </a>
-                </div>
+               <ul className='flex gap-8'>
+                  <li className="inline-flex transition duration-300 ease-in-out border-b-2 border-trabsparent hover:border-orange-button">
+                    {/*<link to='/Servicios/Algoaqui'> className="mt-6 text-lg leading-8 text-gray-600 sm:text-center"*/}
+                      Item1
+                    {/*</link>*/}
+                    
+
+                  </li>
+                  <li className="inline-flex transition duration-300 ease-in-out border-b-2 border-trabsparent hover:border-orange-button">
+                    {/*<link to='/Servicios/Algoaqui'> className="mt-6 text-lg leading-8 text-gray-700 sm:text-center"*/}
+                      Item2
+                    {/*</link>*/}
+
+                  </li>
+               </ul>
+               
               </div>
               <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                 <svg
@@ -74,7 +73,7 @@ function Header(){
             </div>
           </div>
         </div>
-      </main>
+    </main>
     )
 }
 
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -22,11 +22,11 @@ function Navbar(){
                 className=""/>
                 </Link>
                 <div className="ml-4 mt-2 flex-shrink-0">
-                <NavLink to='/Casos' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Casos</NavLink>
-                <NavLink to='/Servicios' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Servicios</NavLink>
-                <NavLink to='/Nosotros' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Nosotros</NavLink>
-                <NavLink to='/About' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">About</NavLink> 
-                <NavLink to='/Blog' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 mx-4">Blog</NavLink>   
+                <NavLink to='/Casos' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Casos</NavLink>
+                <NavLink to='/Servicios' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Servicios</NavLink>
+                <NavLink to='/Nosotros' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Nosotros</NavLink>
+                <NavLink to='/About' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">About</NavLink> 
+                <NavLink to='/Blog' className="text-lg inline-flex  font-medium leading-6 text-gray-900 border-b-2 border-white hover:border-orange-500 transition duration-300 ease-in-out mx-4">Blog</NavLink>   
                 <button
                     type="button"
                     className="ml-12 relative inline-flex items-center rounded-md border border-transparent bg-orange-button px-6 py-2 text-lg font-bold text-white shadow-sm transition duration-300 ease-in-out hover:bg-black focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
